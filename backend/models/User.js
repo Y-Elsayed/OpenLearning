7450bdca417775d8a19866e87069e6a082444a97
Ayman-Marghani <ayman.talaat.marghani@gmail.com>
2025-01-02 15:47:43 -0600

    Updates User schema naming convention of variables

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 // Main User Schema
 const UserSchema = new mongoose.Schema(
   {
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     gender: { type: String, enum: ['male', 'female', 'prefer not to say'], default: null },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }, // email validation
     password: { type: String, required: true },
-    date_of_birth: { type: Date, required: true },
+    dateOfBirth: { type: Date, required: true },
     preferences: { type: [String], default: [] },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
   },
   {
     timestamps: true // Automatically manages `createdAt` and `updatedAt`
